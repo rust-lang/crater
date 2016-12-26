1c5dfdd52619ae9258292d4f780e2cfd60e51ab3
Brian Anderson <banderson@mozilla.com>
2016-12-26 04:47:27 +0000

    Remove old docker code

--- src/docker.rs
+++ src/docker.rs
@@ -80,36 +80,18 @@ pub fn run(source_path: &Path, target_path: &Path, args: &[&str]) -> Result<()> 
 
     log!("running: {}", args.join(" "));
 
-    let source_dir=absolute(source_path);
-    let cargo_home=absolute(Path::new(CARGO_HOME));
-    let rustup_home=absolute(Path::new(RUSTUP_HOME));
-    // This is configured as CARGO_TARGET_DIR by the docker container itself
-    let target_dir=absolute(target_path);
+    let env = RustEnv {
+        args: args,
+        privileged: false,
+        work_dir: (source_path.into(), Perm::ReadOnly),
+        cargo_home: (Path::new(CARGO_HOME).into(), Perm::ReadOnly),
+        rustup_home: (Path::new(RUSTUP_HOME).into(), Perm::ReadOnly),
+        // This is configured as CARGO_TARGET_DIR by the docker container itself
+        target_dir: (target_path.into(), Perm::ReadWrite),
+    };
 
-    fs::create_dir_all(&source_dir);
-    fs::create_dir_all(&cargo_home);
-    fs::create_dir_all(&rustup_home);
-    fs::create_dir_all(&target_dir);
-
-    let test_mount = &format!("{}:/source:ro", source_dir.display());
-    let cargo_home_mount = &format!("{}:/cargo-home:ro", cargo_home.display());
-    let rustup_home_mount = &format!("{}:/rustup-home:ro", rustup_home.display());
-    let target_mount = &format!("{}:/target", target_dir.display());
-
-    let image_name = "cargobomb";
-
-    let user_env = &format!("USER_ID={}", user_id());
-    let cmd_env = &format!("CMD={}", args.join(" "));
-
-    let ref args_ = vec!["-v", test_mount,
-                         "-v", cargo_home_mount,
-                         "-v", rustup_home_mount,
-                         "-v", target_mount,
-                         "-e", user_env,
-                         "-e", cmd_env,
-                         image_name];
-
-    run_in_docker(args_)
+    let c = create_rust_container(&env)?;
+    run_container(&c)
 }
 
 fn absolute(path: &Path) -> PathBuf {

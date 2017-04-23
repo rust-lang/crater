cd41105702f2589a97b8bf58416bdb4ab5e6647c
Corey Farwell <coreyf@rwell.org>
2017-04-23 17:12:30 -0400

    Replace loop containing conditional with `while let`.

--- src/bmk.rs
+++ src/bmk.rs
@@ -13,25 +13,20 @@ pub fn run<S, C>(mut state: S, cmd: C) -> Result<S>
     where C: Process<S>, C: Arguable
 {
     let mut cmds = vec!(cmd);
-    loop {
-        if let Some(cmd) = cmds.pop() {
+    while let Some(cmd) = cmds.pop() {
+        // Round trip through command line argument parsing,
+        // just for testing purpose.
+        let cmd: Vec<String> = cmd.to_args();
+        let cmd: C = Arguable::from_args(cmd)
+            .chain_err(|| "error round-tripping cmd through args")?;
 
-            // Round trip through command line argument parsing,
-            // just for testing purpose.
-            let cmd: Vec<String> = cmd.to_args();
-            let cmd: C = Arguable::from_args(cmd)
-                .chain_err(|| "error round-tripping cmd through args")?;
+        let (state_, new_cmds) = cmd.process(state)?;
+        state = state_;
 
-            let (state_, new_cmds) = cmd.process(state)?;
-            state = state_;
-
-            // Each command execution returns a list of new commands
-            // to execute, in order, before considering the original
-            // complete.
-            cmds.extend(new_cmds.into_iter().rev());
-        } else {
-            break;
-        }
+        // Each command execution returns a list of new commands
+        // to execute, in order, before considering the original
+        // complete.
+        cmds.extend(new_cmds.into_iter().rev());
     }
 
     Ok(state)

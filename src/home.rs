02818f8f6dbe35f5f6c9022f43b24c311cf412d2
Corey Farwell <coreyf@rwell.org>
2017-04-23 20:53:32 -0400

    Rewrite `home` functions to use combinators.

--- src/home.rs
+++ src/home.rs
@@ -5,22 +5,18 @@ use std::env;
 use std::path::PathBuf;
 
 pub fn home_dir() -> Result<PathBuf> {
-    if let Some(d) = env::home_dir() {
-        return Ok(d);
-    }
-    bail!("no home directory");
+    env::home_dir()
+        .ok_or_else(|| "no home directory".into())
 }
 
 pub fn cargo_home() -> Result<PathBuf> {
-    if let Ok(p) = env::var("CARGO_HOME") {
-        return Ok(PathBuf::from(p));
-    }
-    Ok(home_dir()?.join(".cargo"))
+    env::var("CARGO_HOME")
+        .map(PathBuf::from)
+        .or_else(|_| Ok(home_dir()?.join(".cargo")))
 }
 
 pub fn rustup_home() -> Result<PathBuf> {
-    if let Ok(p) = env::var("RUSTUP_HOME") {
-        return Ok(PathBuf::from(p));
-    }
-    Ok(home_dir()?.join(".rustup"))
+    env::var("RUSTUP_HOME")
+        .map(PathBuf::from)
+        .or_else(|_| Ok(home_dir()?.join(".rustup")))
 }

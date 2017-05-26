91b2698e29964b377009ac8246c5cc91327c1123
Tom Prince <tom.prince@twistedmatrix.com>
2017-05-25 20:58:30 -0600

    Use error from crates-index crate.
    This uses frewsxcv/rust-crates-index#15.

--- src/registry.rs
+++ src/registry.rs
@@ -11,12 +11,10 @@ pub fn crates_index_registry() -> Result<crates_index::Index> {
     let index = crates_index::Index::new(repo_path());
     if index.exists() {
         info!("Fetching latest 'crates.io-index' repository commits");
-        index.update().chain_err(|| "Couldn't update crate index.")?;
+        index.update()?;
     } else {
         info!("Cloning 'crates.io-index' repository");
-        index
-            .retrieve()
-            .chain_err(|| "Couldn't fetch crate index.")?;
+        index.retrieve()?;
     }
     Ok(index)
 }

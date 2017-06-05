8765a4c8e818d8321b47fba3ea4e5b4f8b51cb33
Tom Prince <tom.prince@twistedmatrix.com>
2017-06-04 22:21:15 -0600

    Exec into `/run2.sh` in docker image.

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,4 @@ if [ -e /var/run/docker.sock ]; then
     groupmod -g $DOCKER_GROUP_ID docker
     usermod -G docker cargobomb
 fi
-su cargobomb -c "/run2.sh $CMD"
+exec su cargobomb -c "/run2.sh $CMD"

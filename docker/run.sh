57dffdbcd52d1651dbc6e58f9189f86f71bc4d70
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-10-16 23:44:20 +0100

    Docker socket should never be mounted

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 usermod -u "$USER_ID" crater
-if [ -e /var/run/docker.sock ]; then
-    groupmod -g "$DOCKER_GROUP_ID" docker
-    usermod -G docker crater
-fi
 exec su crater -c "/run2.sh $CMD"

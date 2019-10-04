1021b5daed9ccc3e45d554adc33fc6625f651dcc
Pietro Albini <pietro@pietroalbini.org>
2019-10-04 16:35:16 +0200

    ci: remove travis and appveyor

--- ci/run/minicrater-linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-export MINICRATER_SHOW_OUTPUT=1
-export MINICRATER_FAST_WORKSPACE_INIT=1
-
-cargo run -- create-lists
-cargo test -- --ignored --nocapture --test-threads 1

d3c6d84c395d140987f32604e0c63a2d42b11e07
Giacomo Pasini <g.pasini98@gmail.com>
2020-02-29 11:12:23 +0100

    refactor JobDescription into struct

--- src/server/cronjobs.rs
+++ src/server/cronjobs.rs
@@ -7,44 +7,36 @@ use std::thread;
 use std::time::Duration;
 
 const DAY: Duration = Duration::from_secs(60 * 60 * 24);
-// The tuple is composed of:
-// - job name
-// - interval between executions
-// - function to execute at specified intervals
-type JobDescription = (&'static str, Duration, fn(Arc<Data>) -> Fallible<()>);
-
-lazy_static! {
-    static ref JOBS: Vec<JobDescription> = {
-        let mut jobs = Vec::new();
-        jobs.push((
-            "crate list update",
-            DAY,
-            update_crates as fn(Arc<Data>) -> Fallible<()>,
-        ));
-
-        jobs
-    };
+struct JobDescription {
+    name: &'static str,
+    interval: Duration,
+    exec: fn(Arc<Data>) -> Fallible<()>,
 }
+
+static JOBS: &[JobDescription] = &[JobDescription {
+    name: "crates lists update",
+    interval: DAY,
+    exec: update_crates as fn(Arc<Data>) -> Fallible<()>,
+}];
 
 pub fn spawn(data: Data) {
     let data = Arc::new(data);
-    for job in JOBS.iter() {
-        let (name, timer, exec) = job;
+    for job in JOBS {
         // needed to make the borrowck happy
         let data = Arc::clone(&data);
 
         thread::spawn(move || loop {
-            let result = exec(Arc::clone(&data));
+            let result = (job.exec)(Arc::clone(&data));
             if let Err(e) = result {
                 utils::report_failure(&e);
             }
 
             info!(
                 "the {} thread will be respawned in {}s",
-                name,
-                timer.as_secs()
+                job.name,
+                job.interval.as_secs()
             );
-            thread::sleep(*timer);
+            thread::sleep(job.interval);
         });
     }
 }

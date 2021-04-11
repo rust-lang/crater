580db9b7461b226a0fc0068fb293a0e812c08b76
Mark Rousskov <mark.simulacrum@gmail.com>
2021-04-11 12:26:30 -0400

    Log worker failure
    This should help debug early exits, by giving a clear indication of when a
    worker bails out. This should normally never happen; anytime it does also
    decreases our parallelism (as we don't respawn the worker), so should be tracked
    and avoided.

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -101,10 +101,17 @@ pub fn run_ex<DB: WriteResults + Sync>(
         let mut threads = Vec::new();
 
         for worker in &workers {
-            let join = scope
-                .builder()
-                .name(worker.name().into())
-                .spawn(move || worker.run())?;
+            let join =
+                scope
+                    .builder()
+                    .name(worker.name().into())
+                    .spawn(move || match worker.run() {
+                        Ok(()) => Ok(()),
+                        Err(r) => {
+                            log::warn!("worker {} failed: {:?}", worker.name(), r);
+                            Err(r)
+                        }
+                    })?;
             threads.push(join);
         }
         let disk_watcher_thread =
